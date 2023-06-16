96fbe38bacb071f95a12faeb70314008f1589407
chenxuuu <lolicon@papapoi.com>
2023-06-16 15:08:59 +0800

    update: 更新ava版本

--- Image2Display/Image2Display/Program.cs
+++ Image2Display/Image2Display/Program.cs
@@ -1,24 +1,21 @@
-using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.ReactiveUI;
+﻿using Avalonia;
 using System;
 
-namespace Image2Display
-{
-    internal class Program
-    {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+namespace Image2Display;
 
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .LogToTrace()
-                .UseReactiveUI();
-    }
+class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
 }
--- Image2Display/Image2Display/ViewModels/MainWindowViewModel.cs
+++ Image2Display/Image2Display/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Image2Display.ViewModels;
 
-namespace Image2Display.ViewModels
+public partial class MainWindowViewModel : ViewModelBase
 {
-    public class MainWindowViewModel : ViewModelBase
-    {
-        public string Greeting => "Welcome to Avalonia!";
-    }
+    public string Greeting => "Welcome to Avalonia!";
 }
