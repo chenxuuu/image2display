47759b7a3440d78722478f34b00baf44dd3eb8e4
chenxuuu <lolicon@papapoi.com>
2023-07-26 21:14:19 +0800

    update: osx下的字体加上

--- Image2Display/Image2Display/Views/MainWindow.axaml.cs
+++ Image2Display/Image2Display/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using FluentAvalonia.UI.Controls;
 using Image2Display.ViewModels;
 using System.Diagnostics;
 
@@ -9,12 +10,11 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        MainNavigationView.ItemInvoked += MainNavigationView_ItemInvoked;
     }
 
-    private void MainNavigationView_ItemInvoked(object? sender, FluentAvalonia.UI.Controls.NavigationViewItemInvokedEventArgs e)
+    private void MainNavigationView_SelectionChanged(object? sender, FluentAvalonia.UI.Controls.NavigationViewSelectionChangedEventArgs e)
     {
-        var tag = e.InvokedItemContainer.Tag as string;
+        var tag = (string)((NavigationViewItem)e.SelectedItem).Tag!;
         ((MainWindowViewModel)this.DataContext!).ChangePage(tag!);
     }
 }
