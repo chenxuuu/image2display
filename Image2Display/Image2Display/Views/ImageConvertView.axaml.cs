e95b4f14a4cbc0fb9d4ea51c0f289ca6dbf59946
chenxuuu <lolicon@papapoi.com>
2023-07-26 17:24:18 +0800

    add: 多个页面+切换页面的功能

---
+++ Image2Display/Image2Display/Views/ImageConvertView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Image2Display.ViewModels;
+
+namespace Image2Display.Views;
+
+public partial class ImageConvertView : UserControl
+{
+    public ImageConvertView()
+    {
+        InitializeComponent();
+    }
+}
