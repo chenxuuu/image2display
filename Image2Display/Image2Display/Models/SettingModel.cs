70d6a9e24e2abf8d9ce3dee30c4b10db75bb43e3
chenxuuu <lolicon@papapoi.com>
2023-07-28 15:51:27 +0800

    add: 工具库与配置文件逻辑

---
+++ Image2Display/Image2Display/Models/SettingModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Image2Display.Models
+{
+    public class SettingModel
+    {
+        private static string[] _supportLanguages = new string[] { "zh-CN", "en-US" };
+        private string? _language = null;
+        /// <summary>
+        /// 软件语言
+        /// </summary>
+        public string Language
+        {
+            get
+            {
+                if (_language is null)
+                {
+                    var lang = System.Threading.Thread.CurrentThread.CurrentCulture.Name;
+                    if(!_supportLanguages.Contains(lang))//不支持的语言一律用英语
+                        _language = "en-US";
+                    else
+                        _language = lang;
+                }
+                return _language!;
+            }
+            set
+            {
+                _language = value;
+            }
+        }
+    }
+}
---
+++ Image2Display/Image2Display/Utils.cs
@@ -0,0 +1,88 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml.Styling;
+using HarfBuzzSharp;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Image2Display
+{
+    public class Utils
+    {
+        private static string? _version = null;
+        /// <summary>
+        /// 软件版本
+        /// </summary>
+        public static string Version
+        {
+            get
+            {
+                if (_version is null)
+                {
+                    var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version!;
+                    _version = $"{version.Major}.{version.Minor}.{version.Build}";
+                }
+                return _version;
+            }
+        }
+
+        private static string _settingsPath = "settings.json";
+        public static Models.SettingModel? _settings = null;
+        /// <summary>
+        /// 软件配置
+        /// </summary>
+        public static Models.SettingModel Settings
+        {
+            get
+            {
+                if (_settings is null)//初始化
+                {
+                    var builder = new ConfigurationBuilder()
+                        .AddJsonFile(_settingsPath, true, false);
+                    var config = builder.Build();
+                    _settings = config.Get<Models.SettingModel>() ?? new Models.SettingModel();
+                }
+                return _settings!;
+            }
+        }
+
+        /// <summary>
+        /// 启动软件时的初始化操作
+        /// </summary>
+        public static void Initial()
+        {
+            //初始化语言
+            ChangeLanguage(Settings.Language);
+        }
+
+        /// <summary>
+        /// 设置语言
+        /// </summary>
+        /// <param name="language">语言代码，如zh-cn</param>
+        public static void ChangeLanguage(string language)
+        {
+            var file = $"avares://Image2Display/Assets/Languages/{language}.axaml";
+            if (File.Exists(language))
+                file = language;
+            var data =
+                new ResourceInclude(new Uri(file, UriKind.Absolute));
+            data.Source = new Uri(file, UriKind.Absolute);
+            Application.Current!.Resources.MergedDictionaries[0] = data;
+        }
+
+        /// <summary>
+        /// 主动保存配置
+        /// </summary>
+        public static void SaveSettings()
+        {
+            string jsonString = JsonSerializer.Serialize(Settings);
+            File.WriteAllText(_settingsPath, jsonString);
+        }
+    }
+}
