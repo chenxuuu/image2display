e17dca5d00528c6270185971bfbacf9e5b6c90e7
chenxuuu <lolicon@papapoi.com>
2023-07-28 17:10:59 +0800

    fix: 测试框架不能随便升级

---
+++ Image2Display/I2D_Test/UtilsTest.cs
@@ -0,0 +1,47 @@
+﻿using Avalonia;
+using Avalonia.Headless;
+using Avalonia.Headless.XUnit;
+using Image2Display;
+using Image2Display.ViewModels;
+using Image2Display.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I2D_Test
+{
+    public class UtilsTest
+    {
+        public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>()
+            .UseHeadless(new AvaloniaHeadlessPlatformOptions());
+
+        [AvaloniaFact]
+        public void Language()
+        {
+            Utils.Initial();
+            _ = new MainWindow()
+            {
+                DataContext = new MainWindowViewModel(),
+            };
+            Utils.ChangeLanguage("test",true);
+            Utils.ChangeLanguage("en-US", true);
+        }
+
+        [AvaloniaFact]
+        public void Setting()
+        {
+            Utils.Initial();
+            _ = new MainWindow()
+            {
+                DataContext = new MainWindowViewModel(),
+            };
+            Utils.Settings.Language = "en-US";
+            Utils.SaveSettings();
+            Utils.Settings.Language = "zh-CN";
+            Utils.SaveSettings();
+            Assert.Equal("zh-CN", Utils.Settings.Language);
+        }
+    }
+}
