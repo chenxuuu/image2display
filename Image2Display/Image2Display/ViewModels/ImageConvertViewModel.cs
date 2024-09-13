3b34c05b35d96e069a0a305c1a9d4cfebb1f6083
chenxuuu <lolicon@papapoi.com>
2024-09-13 18:15:54 +0800

    add: 导出配置选项和绑定

--- Image2Display/Image2Display/ViewModels/ImageConvertViewModel.cs
+++ Image2Display/Image2Display/ViewModels/ImageConvertViewModel.cs
@@ -5,6 +5,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Image2Display.Helpers;
 using Image2Display.Models;
+using SixLabors.ImageSharp.PixelFormats;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -16,6 +17,46 @@ namespace Image2Display.ViewModels
 {
     public partial class ImageConvertViewModel : ViewModelBase
     {
+        [ObservableProperty]
+        private Bitmap? _rawImage = null;
+        [ObservableProperty]
+        private int _imageColorCount = 0;
+
+        [ObservableProperty]
+        private bool[] _pixelWay = [true, false, false, false, false, false, false, false];
+        [ObservableProperty]
+        private bool[] _isByteShow = [true, false, false];
+        [ObservableProperty]
+        private string[] _byteContent = [
+            "-", "-", "-", "-", "-", "-", "-", "-",
+            "-", "-", "-", "-", "-", "-", "-", "-",
+            "-", "-", "-", "-", "-", "-", "-", "-",
+            "-", "-", "-", "-", "-", "-", "-", "-",
+            ];
+        private static readonly SolidColorBrush GrayColor = new (new Color(0x7f, 0x7f, 0x7f, 0x7f));
+        [ObservableProperty]
+        private Brush[] _byteColor = [
+            GrayColor,GrayColor, GrayColor, GrayColor, GrayColor, GrayColor, GrayColor, GrayColor,
+            GrayColor,GrayColor, GrayColor, GrayColor, GrayColor, GrayColor, GrayColor, GrayColor,
+            GrayColor,GrayColor, GrayColor, GrayColor, GrayColor, GrayColor, GrayColor, GrayColor,
+            GrayColor,GrayColor, GrayColor, GrayColor, GrayColor, GrayColor, GrayColor, GrayColor,
+            ];
+
+        [ObservableProperty]
+        private int _colorMode = 0;
+        [ObservableProperty]
+        private int _colorDepth = 0;
+        [ObservableProperty]
+        private int _fullColorStorage = 2;
+
+        [ObservableProperty]
+        private int _pixelTraversalOrder = 0;
+        [ObservableProperty]
+        private int _byteOrder = 0;
+        [ObservableProperty]
+        private int _colorInternalOrder = 0;
+
+
         public ImageConvertViewModel()
         {
             if (Utils.ImageDataTemp != null)
@@ -26,12 +67,14 @@ namespace Image2Display.ViewModels
             }
             Utils.ImportImageAction = (img) =>
             {
+                Image?.Dispose();
                 Image = img;
                 ImportImage();
             };
         }
 
         private ImageData? Image = null;
+        private List<Rgba32> ImageColors = new List<Rgba32>();
 
 
         /// <summary>
@@ -44,14 +87,30 @@ namespace Image2Display.ViewModels
                 Debug.WriteLine("Image is null");
                 return;
             }
-            var clolrList = Image.GetColors();
-            Debug.WriteLine($"Color Count: {clolrList.Count}");
+            ImageColors = Image.GetColors();
+            Debug.WriteLine($"Color Count: {ImageColors.Count}");
+            ImageColorCount = ImageColors.Count;
+            using var stream = Image.GetStream();
+            RawImage = new Bitmap(stream);
         }
 
 
 
+        [RelayCommand]
+        private async Task CopyAsArrayData()
+        {
 
+        }
+        [RelayCommand]
+        private async Task ExportAsArrayFile()
+        {
 
+        }
+        [RelayCommand]
+        private async Task ExportBinaryFile()
+        {
+
+        }
 
         [RelayCommand]
         private void Test()
